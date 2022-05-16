791f5cf985c615b23d4cd733e1f318240110ad8a
Rico Zhu <rico.zhu@duke.edu>
2022-05-16 12:53:09 -0400

    Added Endpoint Eulers Method

--- eulers.py
+++ eulers.py
@@ -26,4 +26,30 @@ def eulers_method(f, h, t0, y0, n, verbose=True):
 
         i = i + 1
 
-eulers_method(method_2_7_1, 0.1, 0, 1, 4)
+# Finds runs eulers up to a final time tf
+def eulers_endpoint(f, h, t0, y0, tf, verbose=True):
+    
+    i = 0
+    tn = t0
+    yn = y0
+
+    if verbose:
+        print("n: {} tn: {} yn: {} fn: {}".format(i, tn, yn, f(tn, yn)))
+
+    i = i + 1
+
+    while True:
+
+        # Find slope via derivative eq callback
+        yn = yn + f(tn, yn) * h
+        tn = tn + h
+
+        if verbose:
+            print("n: {} tn: {} yn: {} fn: {}".format(i, round(tn, 3), round(yn, 5), round(f(tn, yn), 5)))
+
+        i = i + 1
+
+        if tn >= tf:
+            break
+
+eulers_endpoint(method_2_7_1, 0.05, 0, 1, 0.4)
