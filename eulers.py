266043b913b59fec050630a261653494339dbb14
Rico Zhu <rico.zhu@duke.edu>
2022-05-16 12:50:44 -0400

    Completed Eulers Method

--- eulers.py
+++ eulers.py
@@ -1,23 +1,29 @@
 
-# Methods to be integrated
-def method_2_7_1():
-    pass
+# Methods to be integrated, must have (tn, yn) as input
+def method_2_7_1(tn, yn):
+    return 3 + tn - yn
 
 # Performs euler's method with step h on callback func f for up to n steps
 def eulers_method(f, h, t0, y0, n, verbose=True):
 
+    i = 0
     tn = t0
     yn = y0
-    fn = 0
 
-    for i in range(0, n):
+    if verbose:
+        print("n: {} tn: {} yn: {} fn: {}".format(i, tn, yn, f(tn, yn)))
+
+    i = i + 1
+
+    for i in range(1, n):
 
         # Find slope via derivative eq callback
+        yn = yn + f(tn, yn) * h
         tn = tn + h
-        fn = f(tn, yn)
-        yn = yn + fn * tn
 
         if verbose:
-            print("n: {} tn: {} yn: {} fn: {}".format(n, tn, yn, fn))
+            print("n: {} tn: {} yn: {} fn: {}".format(i, round(tn, 3), round(yn, 5), round(f(tn, yn), 5)))
 
-        n = n + 1
+        i = i + 1
+
+eulers_method(method_2_7_1, 0.1, 0, 1, 4)
