1be0614300cf2ce0be34eca8a3becee213a0c9eb
Rico Zhu <rico.zhu@duke.edu>
2022-05-16 12:35:51 -0400

    Added Eulers Method

--- eulers.py
+++ eulers.py
@@ -3,3 +3,21 @@
 def method_2_7_1():
     pass
 
+# Performs euler's method with step h on callback func f for up to n steps
+def eulers_method(f, h, t0, y0, n, verbose=True):
+
+    tn = t0
+    yn = y0
+    fn = 0
+
+    for i in range(0, n):
+
+        # Find slope via derivative eq callback
+        tn = tn + h
+        fn = f(tn, yn)
+        yn = yn + fn * tn
+
+        if verbose:
+            print("n: {} tn: {} yn: {} fn: {}".format(n, tn, yn, fn))
+
+        n = n + 1
